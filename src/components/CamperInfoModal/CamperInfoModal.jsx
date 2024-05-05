fdcd376545b5d8090d1a6b9860496cdf1c903b98
VuktorTS <skalina222@gmail.com>
2024-05-05 10:47:02 +0300

    add details container

--- src/components/CamperInfoModal/CamperInfoModal.jsx
+++ src/components/CamperInfoModal/CamperInfoModal.jsx
@@ -1,19 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import { useLockBodyScroll } from "react-use";
 import {
   Backdrop,
   ButtonClose,
+  ButtonDetail,
+  CalleryImg,
   CloseIcon,
   Container,
+  Description,
+  DetailsContainer,
+  HeaderModal,
+  IconLocation,
+  IconStar,
+  ImgCamper,
+  ImgContainer,
+  InfoCamper,
+  LocationCamper,
   NameCamper,
-} from "./CamperInfoModalstyled";
+  PriceCamper,
+  ReviewsCamper,
+  TabDetailItem,
+  TabDetailsList,
+} from "./CamperInfoModal.styled";
 import icons from "../../img/sprite.svg";
+import clsx from "clsx";
 
 const modalRoot = document.querySelector("#modal");
 
 const CamperInfoModal = ({ onClose, adverd }) => {
   useLockBodyScroll(true);
+  const [isActive, setIsActive] = useState(true);
 
   useEffect(() => {
     const onEscPress = (e) => {
@@ -35,16 +52,70 @@ const CamperInfoModal = ({ onClose, adverd }) => {
 
   return createPortal(
     <Backdrop onClick={handleClick}>
-      <div>
-        <Container>
-          <ButtonClose type="button" onClick={onClose} aria-label="Close">
-            <CloseIcon>
-              <use href={`${icons}#icon-cross`}></use>
-            </CloseIcon>
-          </ButtonClose>
+      <Container>
+        <ButtonClose type="button" onClick={onClose} aria-label="Close">
+          <CloseIcon>
+            <use href={`${icons}#icon-cross`}></use>
+          </CloseIcon>
+        </ButtonClose>
+        <HeaderModal>
           <NameCamper>{adverd.name}</NameCamper>
-        </Container>
-      </div>
+          <InfoCamper>
+            <ReviewsCamper>
+              <IconStar>
+                <use href={`${icons}#icon-star`}></use>
+              </IconStar>
+              <u>
+                {adverd.rating}({adverd.reviews.length} Reviews)
+              </u>
+            </ReviewsCamper>
+            <LocationCamper>
+              <IconLocation>
+                <use href={`${icons}#icon-map-pin`}></use>
+              </IconLocation>
+              {adverd.location}
+            </LocationCamper>
+          </InfoCamper>
+          <PriceCamper>{adverd.price.toFixed(2)}</PriceCamper>
+        </HeaderModal>
+        <CalleryImg>
+          {adverd.gallery.map((imgUrl) => (
+            <ImgContainer key={imgUrl}>
+              <ImgCamper src={imgUrl} alt={adverd.name} />
+            </ImgContainer>
+          ))}
+        </CalleryImg>
+        <Description>{adverd.description}</Description>
+
+        <TabDetailsList>
+          <TabDetailItem>
+            <ButtonDetail
+              type="button"
+              className={clsx(isActive && "active")}
+              onClick={() => setIsActive(true)}
+            >
+              Features
+            </ButtonDetail>
+          </TabDetailItem>
+          <TabDetailItem>
+            <ButtonDetail
+              type="button"
+              className={clsx(!isActive && "active")}
+              onClick={() => setIsActive(false)}
+            >
+              Reviews
+            </ButtonDetail>
+          </TabDetailItem>
+        </TabDetailsList>
+        <DetailsContainer>
+          {isActive ? (
+            <Features adverd={adverd} />
+          ) : (
+            <Reviews adverd={adverd} />
+          )}
+          <Form />
+        </DetailsContainer>
+      </Container>
     </Backdrop>,
     modalRoot,
   );
---
+++ src/components/CamperInfoModal/CamperInfoModal.styled.js
@@ -0,0 +1,133 @@
+import styled from "styled-components";
+
+export const Backdrop = styled.div`
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: var(--backdrop-color);
+  overflow-y: auto;
+  z-index: 2;
+`;
+
+export const Container = styled.div`
+  display: inline-block;
+  width: 982px;
+  margin: 40px 0;
+  padding: 40px;
+  border-radius: 10px;
+  background-color: #fff;
+  position: relative;
+`;
+export const ButtonClose = styled.button`
+  position: absolute;
+  top: 41px;
+  right: 41px;
+  padding: 0;
+  line-height: 0.6;
+  color: var(--color-main);
+  border: none;
+  background: none;
+  cursor: pointer;
+`;
+export const CloseIcon = styled.svg`
+  width: 32px;
+  height: 32px;
+  stroke: var(--color-main);
+`;
+export const HeaderModal = styled.div``;
+export const NameCamper = styled.h2`
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 1.25;
+  margin-bottom: 10px;
+`;
+export const InfoCamper = styled.div`
+  display: flex;
+  gap: 18px;
+  margin-bottom: 16px;
+`;
+export const ReviewsCamper = styled.p`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+`;
+export const IconStar = styled.svg`
+  width: 16px;
+  height: 16px;
+  fill: var(--color-rating);
+  stroke: var(--color-rating);
+`;
+export const LocationCamper = styled.p`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+`;
+export const IconLocation = styled.svg`
+  width: 16px;
+  height: 16px;
+  fill: transparent;
+  stroke: var(--color-main);
+`;
+export const PriceCamper = styled.p`
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 1.25;
+  margin-bottom: 24px;
+`;
+export const CalleryImg = styled.div`
+  display: flex;
+  gap: 16px;
+  margin-bottom: 24px;
+`;
+export const ImgContainer = styled.div`
+  width: 290px;
+  height: 310px;
+`;
+export const ImgCamper = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  object-fit: cover;
+`;
+export const Description = styled.p`
+  color: var(--color-text);
+  margin-bottom: 44px;
+`;
+export const TabDetailsList = styled.ul`
+  position: relative;
+  display: flex;
+  gap: 40px;
+  margin-bottom: 68px;
+  &::after {
+    content: "";
+    position: absolute;
+    background-color: rgba(16, 24, 40, 0.2);
+    top: 40px;
+    height: 1px;
+    width: 100%;
+    border-radius: 1px;
+    display: block;
+  }
+`;
+export const TabDetailItem = styled.li``;
+export const ButtonDetail = styled.button`
+  position: relative;
+  &.active {
+    &::after {
+      content: "";
+      position: absolute;
+      background-color: var(--color-button);
+      top: 38px;
+      height: 5px;
+      width: 100%;
+      border-radius: 2px;
+      display: block;
+      z-index: 99;
+    }
+  }
+`;
+export const DetailsContainer = styled.div``;
