ab784365dae58f63d7ca831ccd71f4bfe76359ba
ViktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-05-07 13:26:44 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import {  Navigate, Route, Routes } from "react-router-dom";
 import { Layout } from "./components/Layout/Layout";
 import { HomePage } from "./page/HomePage";
 import { CatalogPage } from "./page/CatalogPage";
