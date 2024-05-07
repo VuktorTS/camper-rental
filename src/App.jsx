d70257f30aa1553bd6eb6ff61ef04ab1c12b27f1
ViktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-05-07 14:00:02 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export const App = () => {
           <Route path="catalog" element={<CatalogPage />} />
           <Route path="favorites" element={<FavoritesPage />} />
         </Route>        
-        <Route path="*" element={<Navigate to='/' replace />} />
+        <Route path="*" element={<Navigate to='/' replace={true} />} />
       </Routes>
     </Suspense>
   );
